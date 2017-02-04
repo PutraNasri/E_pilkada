da3b719feb8b058031afcff8fc134dd477bb5821
PutraNasri <nasrisyawaldi@gmail.com>
2017-02-04 09:39:11 +0700

    040217

---
+++ app/src/main/java/com/example/byonge/e_pilkada/pilih.java
@@ -0,0 +1,13 @@
+package com.example.byonge.e_pilkada;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class pilih extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_pilih);
+    }
+}
